738bcf84313ad10d3f916874ed7d4eecb587e3f7
Tom Marx <tppm@capgroup.com>
2020-08-25 11:30:29 -0700

    Testing pretraffichook function

--- sam-deploy-demo/tests/deployment/pre_traffic_hook_tests.py
+++ sam-deploy-demo/tests/deployment/pre_traffic_hook_tests.py
@@ -5,25 +5,18 @@ def lambda_handler(event, context):
 
 	print('Invoking pretraffic tests.')
 
-	deployment_id = ''.join(event['DeploymentId'])
-	execution_id = ''.join(event['LifecycleEventHookExecutionId'])
+	# deployment_id = ''.join(event['DeploymentId'])
+	# execution_id = ''.join(event['LifecycleEventHookExecutionId'])
 
-	print('Event:')
-	print(event)
-
-	print('Deployment id:')
-	print(deployment_id)
-	print(len(deployment_id))
-	print('Execution id:')
-	print(execution_id)
-	print(len(execution_id))
+	deployment_id = event['DeploymentId']
+	execution_id = event['LifecycleEventHookExecutionId']
 
 	deploy_client = boto3.client('codedeploy')
 
 	response = deploy_client.put_lifecycle_event_hook_execution_status(
     	deploymentId = deployment_id,
     	lifecycleEventHookExecutionId = execution_id,
-    	status = 'Succeeded'
+    	status = 'Failed'
 	)
 
 	return 'Pretraffic hook completed'
