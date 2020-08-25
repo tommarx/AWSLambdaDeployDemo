1e70c1629cff5730b8349c2f6328e45b06bf7f4e
Tom Marx <tppm@capgroup.com>
2020-08-25 13:34:39 -0700

    Testing pretraffichook function

--- sam-deploy-demo/tests/deployment/pre_traffic_hook_tests.py
+++ sam-deploy-demo/tests/deployment/pre_traffic_hook_tests.py
@@ -5,15 +5,15 @@ def lambda_handler(event, context):
 
 	print('Invoking pretraffic tests.')
 
-	# deployment_id = ''.join(event['DeploymentId'])
-	# execution_id = ''.join(event['LifecycleEventHookExecutionId'])
-
 	deployment_id = event['DeploymentId']
 	execution_id = event['LifecycleEventHookExecutionId']
 
 	deploy_client = boto3.client('codedeploy')
 
-	print(process)
+	print("Context:")
+	print(context)
+	print("Event:")
+	print(event)
 
 	response = deploy_client.put_lifecycle_event_hook_execution_status(
     	deploymentId = deployment_id,
